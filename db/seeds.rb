5f43fdc018c88d76a754cfef5980c465f7dec046
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-15 20:36:59 -0400

    Making movie pictures show at /movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create!(
+	:director => 'Steven Spielberg',
+	:title 		=> 'E.T.',
+	:url 			=> 'http://beyondthemarquee.com/wp-content/uploads/2011/10/Loc-ETBTM45.bmp'
+)
+Movie.create!(
+	:director => 'Steven Spielberg',
+	:title		=> 'Jurassic Park',
+	:url 			=> 'http://img4.wikia.nocookie.net/__cb20141015011537/jurassicpark/images/c/ce/JP-MoviePoster.jpg'
+)
+
